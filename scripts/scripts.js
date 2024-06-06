2d8c3613179e67b451c369f4eb0a6b273f4cf9a9
KP Parrish <krparrish@adobe.com>
2024-06-06 13:19:39 -0600

    updated scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -158,6 +158,34 @@ async function loadFonts() {
   }
 }
 
+/**
+ * to add/remove a template, just add/remove it in the list below
+ */
+const TEMPLATE_LIST = [
+];
+
+/**
+ * Run template specific decoration code.
+ * @param {Element} main The container element
+ */
+async function decorateTemplates(main) {
+  try {
+    const template = getMetadata('template');
+    const templates = TEMPLATE_LIST;
+    if (templates.includes(template)) {
+      const mod = await import(`../templates/${template}/${template}.js`);
+      loadCSS(`${window.hlx.codeBasePath}/templates/${template}/${template}.css`);
+      if (mod.default) {
+        await mod.default(main);
+      }
+    }
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error('Auto Blocking failed', error);
+  }
+}
+
+
 function autolinkModals(element) {
 	element.addEventListener('click', async (e) => {
 	  const origin = e.target.closest('a');
@@ -341,10 +369,22 @@ async function loadEager(doc) {
 
 	const main = doc.querySelector('main');
 	if (main) {
+	  decorateTemplates(main);
 	  decorateMain(main);
+	  aggregateTabSectionsIntoComponents(main);
 	  document.body.classList.add('appear');
 	  await waitForLCP(LCP_BLOCKS);
 	}
+
+	/**
+   * fix UE meta tag
+   */
+	  doc.querySelectorAll('meta').forEach((m) => {
+		const prop = m.getAttribute('property');
+		if (prop && prop.startsWith('urn:adobe')) {
+		  m.setAttribute('content', `aem:${m.getAttribute('content')}`);
+		}
+	  });
   
 	try {
 	  /* if desktop (proxy for fast connection) or fonts already loaded, load fonts.css */
@@ -377,6 +417,20 @@ async function loadLazy(doc) {
   sampleRUM('lazy');
   sampleRUM.observe(main.querySelectorAll('div[data-block-name]'));
   sampleRUM.observe(main.querySelectorAll('picture > img'));
+
+await window.hlx.plugins.run('loadLazy', pluginContext);
+
+  // Mark customer as having viewed the page once
+  localStorage.setItem('franklin-visitor-returning', true);
+
+  const context = {
+    getMetadata,
+    toClassName,
+  };
+
+  // eslint-disable-next-line import/no-relative-packages
+  const { initConversionTracking } = await import('../plugins/rum-conversion/src/index.js');
+  await initConversionTracking.call(context, document);
 }
 
 /**
@@ -388,6 +442,115 @@ function loadDelayed() {
   window.setTimeout(() => import('./delayed.js'), 3000);
   // load anything that can be postponed to the latest here
 }
+
+export function addAnchorLink(elem) {
+	const link = document.createElement('a');
+	link.setAttribute('href', `#${elem.id || ''}`);
+	link.setAttribute('title', `Copy link to "${elem.textContent}" to clipboard`);
+	link.classList.add('anchor-link');
+	link.addEventListener('click', (e) => {
+	  e.preventDefault();
+	  navigator.clipboard.writeText(link.href);
+	  window.location.href = link.href;
+	  e.target.classList.add('anchor-link-copied');
+	  setTimeout(() => e.target.classList.remove('anchor-link-copied'), 1000);
+	});
+	link.innerHTML = elem.innerHTML;
+	elem.innerHTML = '';
+	elem.append(link);
+  }
+
+  export async function fetchJson(href) {
+	const url = new URL(href);
+	try {
+	  const resp = await fetch(
+		url,
+		{
+		  headers: {
+			'Content-Type': 'text/html',
+		  },
+		  method: 'get',
+		  credentials: 'include',
+		},
+	  );
+	  const error = new Error({
+		code: 500,
+		message: 'login error',
+	  });
+	  if (resp.redirected) throw (error);
+  
+	  return resp.json();
+	} catch (error) {
+	  return error;
+	}
+  }
+
+  export async function useGraphQL(query, param) {
+	const configPath = `${window.location.origin}/demo-config.json`;
+	let { data } = await fetchJson(configPath);
+	data = data && data[0];
+	if (!data) {
+	  console.log('config not present'); // eslint-disable-line no-console
+	  return;
+	}
+	const { origin } = window.location;
+  
+	if (origin.includes('.live')) {
+	  data['aem-author'] = data['aem-author'].replace('author', data['hlx.live']);
+	} else if (origin.includes('.page')) {
+	  data['aem-author'] = data['aem-author'].replace('author', data['hlx.page']);
+	}
+	data['aem-author'] = data['aem-author'].replace(/\/+$/, '');
+	const { pathname } = new URL(query);
+	const url = param ? new URL(`${data['aem-author']}${pathname}${param}`) : new URL(`${data['aem-author']}${pathname}`);
+	const options = data['aem-author'].includes('publish')
+	  ? {
+		headers: {
+		  'Content-Type': 'text/html',
+		},
+		method: 'get',
+	  }
+	  : {
+		headers: {
+		  'Content-Type': 'text/html',
+		},
+		method: 'get',
+		credentials: 'include',
+	  };
+	try {
+	  const resp = await fetch(
+		url,
+		options,
+	  );
+  
+	  const error = new Error({
+		code: 500,
+		message: 'login error',
+	  });
+  
+	  if (resp.redirected) throw (error);
+  
+	  const adventures = await resp.json();
+	  const environment = data['aem-author'];
+	  return { adventures, environment }; // eslint-disable-line consistent-return
+	} catch (error) {
+	  console.log(JSON.stringify(error)); // eslint-disable-line no-console
+	}
+  }
+  
+  export function addElement(type, attributes, values = {}) {
+	const element = document.createElement(type);
+  
+	Object.keys(attributes).forEach((attribute) => {
+	  element.setAttribute(attribute, attributes[attribute]);
+	});
+  
+	Object.keys(values).forEach((val) => {
+	  element[val] = values[val];
+	});
+  
+	return element;
+  }
 
 async function loadPage() {
   await loadEager(document);
